e02303ccc5ffcae257377a190a23c3f2cfccf4cc
Robert Savage <robert@shadeblue.com>
2019-09-10 11:22:06 -0400

    updated Java module names to conform to standards.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -24,16 +24,16 @@
  * limitations under the License.
  * #L%
  */
-module pi4j.demo.telegraph {
+module com.pi4j.demo.telegraph {
 
     // Pi4J MODULES
-    requires pi4j.api;
-    requires pi4j.plugin.pigpio;
+    requires com.pi4j;
+    requires com.pi4j.plugin.pigpio;
 
     // SLF4J MODULES
     requires org.slf4j;
     requires org.slf4j.simple;
 
     // allow access to classes in the following namespaces for Pi4J annotation processing
-    opens com.pi4j.demo.telegraph;
+    opens com.pi4j.demo.telegraph to com.pi4j;
 }
