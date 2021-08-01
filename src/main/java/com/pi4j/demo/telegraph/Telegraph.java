6b86c1c218b647ef40535d8fd29c88391fca5893
vicwintriss <sdvic@me.com>
2021-08-01 13:22:55 -0700

    Version G trying to add enable

--- src/main/java/com/pi4j/demo/telegraph/Telegraph.java
+++ src/main/java/com/pi4j/demo/telegraph/Telegraph.java
@@ -1,149 +1,94 @@
 package com.pi4j.demo.telegraph;
 /*-
- * #%L
  * **********************************************************************
  * ORGANIZATION  :  Pi4J
- * PROJECT       :  Pi4J :: DEMO  :: Telegraph Demo
+ * PROJECT       :  Pi4J :: DEMO  :: Telegraph Demo version 9.0.H
  * FILENAME      :  Telegraph.java
+ * Run with:  sudo java --module-path . --module com.pi4j.demo.telegraph/com.pi4j.demo.telegraph.Telegraph
+ * Transfer directory to Pi with: scp -r "${PWD}" pi@raspberrypi.local:
+ * Clear directory with: rm -r "${PWD}"
+ * Files in Pi are at: /Users/vicwintriss/git/pi4j-example-telegraph/target/distribution
  *
- * This file is part of the Pi4J project. More information about
- * this project can be found here:  https://pi4j.com/
  * **********************************************************************
- * %%
- * Copyright (C) 2012 - 2019 Pi4J
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
  */
-
 import com.pi4j.Pi4J;
 import com.pi4j.event.ShutdownListener;
-import com.pi4j.io.binding.OnOffBinding;
-import com.pi4j.io.gpio.digital.*;
-import com.pi4j.io.group.OnOffGroup;
-import com.pi4j.io.pwm.Pwm;
-import com.pi4j.io.pwm.PwmType;
-
-/**
- * <h2>Telegraph Sample</h2>
- * <p>This example uses standard and straight-forward/plain-old Java code to utilize Pi4J.</p>
- * <p>This project is available on <a href="https://github.com/Pi4J/pi4j-demo-telegraph">GitHub</a></p>
- *
- * @author Robert Savage (<a href="http://www.savagehomeautomation.com">http://www.savagehomeautomation.com</a>)
- * @version $Id: $Id
- */
-public class Telegraph {
-
-    public static final int TELEGRAPH_KEY_PIN     = 04;  // DIGITAL INPUT PIN
-    public static final int PWM_PIN_RIGHT         = 18;  // PWM CHANNEL 0 (RIGHT)
-    public static final int PWM_PIN_LEFT          = 19;  // PWM CHANNEL 1 (LEFT)
-    public static final int TELEGRAPH_SOUNDER_PIN = 23;  // DIGITAL OUTPUT PIN
-    public static final int LED_PIN               = 25;  // DIGITAL OUTPUT PIN
-
-    /**
-     * <p>main.</p>
-     *
-     * @param args an array of {@link String} objects.
-     * @throws Exception if any.
-     */
-    public static void main(String[] args) throws Exception {
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalState;
+public class Telegraph
+{
+    public static final int STEP_PIN = 27;  // DIGITAL OUTPUT STEP PIN IO27 (connector pin 13)
+    public static final int DIR_PIN = 17;  // DIGITAL OUTPUT DIR PIN IO17 (connector pin 11)
+    public static final int ENABLE_PIN = 22;  // DIGITAL OUTPUT DIR PIN IO17 (connector pin 11)
+    public static void main(String[] args) throws Exception
+    {
         // configure default lolling level, accept a log level as the fist program argument
         System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "INFO");
-
         // instruct PIGPIO which remote Raspberry Pi to connect to
         //System.setProperty("pi4j.host", "127.0.0.1");
         //System.setProperty("pi4j.pigpio.remote", "false");
-
         // Initialize Pi4J with an auto context
         var pi4j = Pi4J.newAutoContext();
-
-        // create PWM config
-        var pwmConfig = Pwm.newConfigBuilder(pi4j)
-                .pwmType(PwmType.HARDWARE)
-                .dutyCycle(50)  // 50%
-                .frequency(800) // 800Hz
-                .shutdown(0)
-                .initial(0)
-                .provider("pigpio-pwm");
-
-        // create a DIN config for Telegraph Key
-        var keyConfig = DigitalInput.newConfigBuilder(pi4j)
-                .id("key")
-                .name("Telegraph Key")
-                .address(TELEGRAPH_KEY_PIN)
-                .pull(PullResistance.PULL_DOWN)
-                .debounce(3000L)
-                .provider("pigpio-digital-input");
-
-        // create a digital input config Telegraph Sounder
-        var sounderConfig = DigitalOutput.newConfigBuilder(pi4j)
-                .id("sounder")
-                .name("Telegraph Sounder")
-                .address(TELEGRAPH_SOUNDER_PIN)
-                .shutdown(DigitalState.LOW)
-                .initial(DigitalState.LOW)
-                .provider("pigpio-digital-output");
-
-
-        // create a DIN config Telegraph Sounder
         var ledConfig = DigitalOutput.newConfigBuilder(pi4j)
-                .id("led")
+                .id("step")
                 .name("Telegraph LED Flasher")
-                .address(LED_PIN)
+                .address(STEP_PIN)
+                .shutdown(DigitalState.LOW)
+                .initial(DigitalState.LOW)
+                .provider("pigpio-digital-output");
+        var stepConfig = DigitalOutput.newConfigBuilder(pi4j)
+                .id("dir")
+                .name("Telegraph LED Flasher")
+                .address(DIR_PIN)
                 .shutdown(DigitalState.LOW)
                 .initial(DigitalState.LOW)
                 .provider("pigpio-digital-output");
 
-        // create two hardware PWM instances (LEFT and RIGHT audio channels)
-        var left  = pi4j.create(pwmConfig.address(PWM_PIN_LEFT).id("left-audio-channel"));
-        var right = pi4j.create(pwmConfig.address(PWM_PIN_RIGHT).id("right-audio-channel"));
-
-        // create a digital input pin instance for the Telegraph Key
-        var key = pi4j.create(keyConfig);
-
-        // create a digital input pin instance for the Telegraph Sounder
-        var sounder = pi4j.create(sounderConfig);
-
-        // create a digital output pin instance for the LED
-        //var led = dout.create(ledConfig);
-        var led = pi4j.create(ledConfig);
-
-        // create a group of IO instances that can all be controlled together
-        OnOffGroup signal = OnOffGroup.newInstance(sounder, led, left, right);
-
-        // bind the input changes from the Telegraph Key to the output Signal group
-        key.bind(OnOffBinding.newInstance(signal));
-
-        // add event listener for the Telegraph Key input changes
-        key.addListener((DigitalStateChangeListener) event -> System.out.println("TELEGRAPH DEMO :: " + event));
-
+        var enableConfig = DigitalOutput.newConfigBuilder(pi4j)
+                .id("enable")
+                .name("Telegraph LED Flasher")
+                .address(DIR_PIN)
+                .shutdown(DigitalState.LOW)
+                .initial(DigitalState.LOW)
+                .provider("pigpio-digital-output");
+        var step = pi4j.create(ledConfig);
+        var dir = pi4j.create(stepConfig);
+        var enable = pi4j.create(enableConfig);
         System.out.println("---------------------------------------------------");
-        System.out.println(" [Pi4J V.2 DEMO] TELEGRAPH (Using Plain Old Java)");
+        System.out.println(" Version 9.0.H-VERSION");
         System.out.println("---------------------------------------------------");
         pi4j.registry().describe().print(System.out);
         System.out.println("---------------------------------------------------");
-        System.out.println(" Press the telegraph key when ready.");
-
+        step.high();
+        dir.high();
+        enable.low();//Enable stepper motor
+        for (int i = 0; i < 500; i++)
+        {
+            System.out.print(i + " ");
+            if (step.equals(DigitalState.HIGH))
+            {
+                step.low();
+                System.out.print(" STEP low ");
+            }
+            else
+            {
+                step.high();
+                System.out.println(" STEP high");
+            }
+            Thread.sleep(10);
+            if (i % 10 == 0)
+            {
+                System.out.println("dir change,  " );
+                dir.toggle();
+            }
+        }
         // add a shutdown listener
         pi4j.addListener((ShutdownListener) shutdownEvent -> {
+            enable.high();//Remove power from stepper motor
             System.out.println("---------------------------------------------------");
             System.out.println("[Pi4J V.2 DEMO] SHUTTING DOWN");
             System.out.println("---------------------------------------------------");
         });
-
-        // keep the program running until we see user input
-        System.in.read();
-
         // shutdown Pi4J context now
         pi4j.shutdown();
     }
